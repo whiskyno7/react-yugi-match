38e40a637973d1be1169cebd3c698b5abea1157b
whiskyno7 <84514765+whiskyno7@users.noreply.github.com>
2021-11-18 12:22:32 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  { src: '/img/blueeyes.jpg', matched: false },
+  { src: '/img/heart.jpg', matched: false },
+  { src: '/img/medicine.jpg', matched: false },
+  { src: '/img/rose.jpg', matched: false },
+  { src: '/img/skullguard.jpg', matched: false },
+  { src: '/img/warrior.jpg', matched: false },
+];
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState([0]);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Yugi-oh Match</h1>
+      <div className="ngwrap">
+        <button onClick={shuffleCards}>New Game</button>
+        <p>Turns: {turns}</p>
+      </div>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
